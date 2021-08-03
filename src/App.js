7df6f745329c22a88cfc97ce09796251fb763b2b
Getsuga743 <ivan_maier@hotmail.com>
2021-08-03 01:15:09 -0300

    feat: Add basic Button component and app styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+import Button from './components/button/Button';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="app">
+      <Button>Search for places</Button>
     </div>
   );
 }
