fe6f5a8ad55380e2e867b283a7e04a6acef8ce00
ivanmaierg <ivan_maier@hotmail.com>
2021-08-17 22:29:49 -0300

    fix: Change name of component "PreviewPanel" to "AsidePanel"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './style.css';
-
-import CallToAction from './components/Buttons/CallToAction';
-import GeoButton from './components/Buttons/GeoButton';
-import PreviewPanel from './components/AsidePanel';
+import AsidePanel from './components/AsidePanel';
 import Main from './components/Main';
-
+import response from './utils/resMock.json'
 export default function App() {
+  console.log(response)
   return (
     <div className="app">
-        <PreviewPanel />
+        <AsidePanel />
         <Main />
-        
     </div>
   );
 }
