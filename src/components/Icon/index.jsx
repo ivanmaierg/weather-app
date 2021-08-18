6d26c622d4741e99e6a629fa48581fb603e5503b
ivanmaierg <ivan_maier@hotmail.com>
2021-08-18 15:48:19 -0300

    feat: Add error default image to Icon component

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,13 +1,13 @@
 import useDynamicSVGImport from '@/hooks/useDynamicImportSvg';
 import React from 'react'
-
+import brokenImage from '@icons/brokenImage.svg';
 const Icon = ({ name, onCompleted, onError, ...rest }) => {
     const { error, loading, SvgIcon } = useDynamicSVGImport(name, {
         onCompleted,
         onError
     });
     if (error) {
-        return error.message;
+        return <img src={brokenImage} width={'100%'} height={'auto'} />;
     }
     if (loading) {
         return "Loading...";
